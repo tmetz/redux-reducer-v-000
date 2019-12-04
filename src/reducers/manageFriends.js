584773b20ce0d83f01d1a9f46612c2f6792da700
Tammy Metz <tammy@tammymetz.com>
2019-12-04 11:20:14 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,9 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {...friends, action.friend}
+    case: 'REMOVE_FRIEND':
+      return {}
+    
+  }
 }
